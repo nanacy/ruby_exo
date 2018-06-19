fbe7b7ad1af13ec64ab68021d57ef7f2b84e8d12
nancy <nancy@nancy.VivoBook>
2018-06-19 22:39:16 +0200

    exo5

---
+++ exo_05.rb
@@ -0,0 +1,42 @@
+puts "On va compter le nombre d'heures de travail à THP"
+# ecris la phrase
+#
+puts "Travail : #{10 * 5 * 11}"
+# affiche "Travail" puis calcul les 3 nombres et affiche resultar
+#
+puts "En minutes ça fait : #{10 * 5 * 11 * 60}"
+# affiche "En minutes ça fait :" puis calcul les 4nombres et afiche resultat
+#
+puts "Et en secondes ?"
+# affiche phrase
+#
+puts 10 * 5 * 11 * 60 * 60
+# affiche le resultat du calcul des 5 nombres
+#
+puts "Est-ce que c'est vrai que 3 + 2 < 5 - 7 ?"
+# affiche la phrase
+#
+puts 3 + 2 < 5 - 7
+# calcul et affiche le resultat
+#
+puts "Ça fait combien 3 + 2 ? #{3 + 2}"
+# affiche phrase puis calcule 3+2 et affiche resultat
+#
+puts "Ça fait combien 5 - 7 ? #{5 - 7}"
+# affiche phrase puis calcul 5-7 et affiche resultat
+#
+puts "Ok, c'est faux alors !"
+# affiche phrase
+#
+puts "C'est drôle ça, faisons-en plus :"
+# affiche phrase
+#
+puts "Est-ce que 5 est plus grand que -2 ? #{5 > -2}"
+# affiche phrase puis affiche si (5 est superieur à -2) avec true ou false 
+#
+puts "Est-ce que 5 est supérieur ou égal à -2 ? #{5 >= -2}"
+# affiche phrase puis affiche si (5 est superieure ou egale à -2) avec true ou false 
+#
+puts "Est-ce que 5 est inférieur ou égal à -2 ? #{5 <= -2}"
+# affiche phrase puis affiche si (5 est inferieur ou egale à -2) avec true ou false 
+#
