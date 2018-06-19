8af80b23d8384a3f8ffde061200e2e5627d1e28b
nancy <nancy@nancy.VivoBook>
2018-06-19 22:50:46 +0200

    exo6

---
+++ exo_06.rb
@@ -0,0 +1,12 @@
+number_of_hours_worked_per_day = 10
+number_of_days_worked_per_week = 5
+number_of_weeks_in_THP = 11
+
+puts "Travail : #{number_of_hours_worked_per_day * number_of_days_worked_per_week * number_of_weeks_in_THP}"
+
+# affiche "travail"
+# calcul les 3 variables et affiche le resultat
+
+puts "Et en minutes ça fait : #{number_of_minutes_in_an_hour * number_of_hours_worked_per_day * number_of_days_worked_per_week * number_of_weeks_in_THP}"
+
+# affiche une erreur car une des variables n'a pas été instancié, elle n'existe pas
